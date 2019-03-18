77e6e3b07b6d24dc1f7debbd76085a2b7e7fa15d
kd333 <31936653+FeatheredScorpion@users.noreply.github.com>
2019-03-18 18:36:29 -0400

    Updated netids and project name

--- app/irsystem/controllers/search_controller.py
+++ app/irsystem/controllers/search_controller.py
@@ -2,8 +2,8 @@ from . import *
 from app.irsystem.models.helpers import *
 from app.irsystem.models.helpers import NumpyEncoder as NumpyEncoder
 
-project_name = "Ilan's Cool Project Template"
-net_id = "Ilan Filonenko: if56"
+project_name = "Rocktails"
+net_id = "Kerri Diamond: kd333, William Lin: wrl48, Nathaniel Schickler: njs59, Kaitlin Green: kg345, Megan Rochlin: mjr395"
 
 @irsystem.route('/', methods=['GET'])
 def search():
