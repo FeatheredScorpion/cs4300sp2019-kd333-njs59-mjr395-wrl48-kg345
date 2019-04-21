117d026bb1f22332290e36e6740bb72207ec0f58
william <wrl48@cornell.edu>
2019-04-21 17:39:27 -0400

    Added skeleton code for boolean search (not implemented, from homework assignments)

--- app/__init__.py
+++ app/__init__.py
@@ -10,6 +10,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_socketio import SocketIO
 import sys
 import pickle, re
+from nltk.tokenize import TreebankWordTokenizer
 
 # Configure app
 socketio = SocketIO()
@@ -36,6 +37,9 @@ app.register_blueprint(irsystem)
 
 # Initialize app w/SocketIO
 socketio.init_app(app)
+
+#treebank tokenizer for boolean search
+treebank_tokenizer = TreebankWordTokenizer()
 
 
 # HTTP error handling
@@ -107,3 +111,31 @@ def return_good_types():
         if words[word] > 1:
             good_words.append(word)
     return json.dumps(good_words)
+
+# returns a list of resulting elements that satisfies the boolean search
+@app.route("/booleanSearch/", methods=['GET', 'POST'])
+def boolean_search(query, index=inverted_index, tokenizer=treebank_tokenizer):
+    SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
+    json_url = os.path.join(SITE_ROOT, "static", "drinks.json")
+    data = json.load(open(json_url))
+
+    ans = []
+    temp1 = set()
+    temp2 = set()
+    tokenized = tokenizer.tokenize(query.lower())
+    count = 0
+    for x in tokenized:
+        temp2 = set([a[0] for a in index[x]])
+        if(count == 0):
+            ans = list(temp2)
+            count+=1
+            continue
+        if(len(ans) == 0):
+            return ans
+        if(len(x) == 1 and x in string.punctuation):
+            count+=1
+            continue
+        else:
+            temp1 = set(ans)
+            ans = list(temp2.intersection(temp1))
+    return ans
