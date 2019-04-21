93910e4f9f09d056246a073c40cf1a93071efe0e
Nate <njs59@cornell.edu>
2019-04-21 17:07:32 -0400

    Reformatting

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
 # Gevent needed for sockets
 from gevent import monkey
+
 monkey.patch_all()
 
 # Imports
@@ -10,8 +11,6 @@ from flask_socketio import SocketIO
 import sys
 import pickle, re
 
-
-
 # Configure app
 socketio = SocketIO()
 app = Flask(__name__)
@@ -21,27 +20,28 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 # DB
 db = SQLAlchemy(app)
 
-#inverted index
+# inverted index
 inverted_index = {}
-#good types
+# good types
 good_words = []
 reverse_index_good_words = {}
 
 # Import + Register Blueprints
 from app.accounts import accounts as accounts
+
 app.register_blueprint(accounts)
 from app.irsystem import irsystem as irsystem
+
 app.register_blueprint(irsystem)
 
 # Initialize app w/SocketIO
 socketio.init_app(app)
 
+
 # HTTP error handling
 @app.errorhandler(404)
 def not_found(error):
-  return render_template("404.html"), 404
-
-
+    return render_template("404.html"), 404
 
 
 def tokenize(text):
@@ -51,65 +51,59 @@ def tokenize(text):
     Returns: List
     """
     return [x for x in re.findall(r"[a-z]*", text.lower()) if x != ""]
+
+
 #
 
-#precompute inverted_index  and good types
+# precompute inverted_index  and good types
 @app.route("/hello/", methods=['GET', 'POST'])
 def hello():
-	SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
-	json_url = os.path.join(SITE_ROOT, "static", "drinks.json")
-	data = json.load(open(json_url))
+    SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
+    json_url = os.path.join(SITE_ROOT, "static", "drinks.json")
+    data = json.load(open(json_url))
 
-	#create good types matrix
-	words = {}
-	for drink in data["drinks"]:
-		tokens = tokenize(drink["description"])
-		for token in tokens:
-			if token not in words:
-				words[token] = 0
-			words[token] += 1
-			if token not in inverted_index:
-				inverted_index[token] = []
-			inverted_index[token].append(drink["name"])
-	print(inverted_index)
-	good_words = []
-	index = 0
-	for word in words:
-	    if words[word] > 1:
-	        good_words.append(word)
-	        reverse_index_good_words[word] = index
-	        index += 1
-	#construct inverted index
-	#print(good_words)
-	return redirect('/')
+    # create good types matrix
+    words = {}
+    for drink in data["drinks"]:
+        tokens = tokenize(drink["description"])
+        for token in tokens:
+            if token not in words:
+                words[token] = 0
+            words[token] += 1
+            if token not in inverted_index:
+                inverted_index[token] = []
+            inverted_index[token].append(drink["name"])
+    print(inverted_index)
+    good_words = []
+    index = 0
+    for word in words:
+        if words[word] > 1:
+            good_words.append(word)
+            reverse_index_good_words[word] = index
+            index += 1
+    # construct inverted index
+    # print(good_words)
+    return redirect('/')
 
 
-
-#returns an array of good types
+# returns an array of good types
 @app.route("/good-types/", methods=['GET', 'POST'])
 def return_good_types():
-	SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
-	json_url = os.path.join(SITE_ROOT, "static", "drinks.json")
-	data = json.load(open(json_url))
+    SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
+    json_url = os.path.join(SITE_ROOT, "static", "drinks.json")
+    data = json.load(open(json_url))
 
-	#good types
-	good_words = []
-	words = {}
-	for drink in data["drinks"]:
-		tokens = tokenize(drink["description"])
-		for token in tokens:
-			if token not in words:
-				words[token] = 0
-			words[token] += 1
-	good_words = []
-	for word in words:
-	    if words[word] > 1:
-	        good_words.append(word)
-	return json.dumps(good_words)
-
-
-
-
-
-
-
+    # good types
+    good_words = []
+    words = {}
+    for drink in data["drinks"]:
+        tokens = tokenize(drink["description"])
+        for token in tokens:
+            if token not in words:
+                words[token] = 0
+            words[token] += 1
+    good_words = []
+    for word in words:
+        if words[word] > 1:
+            good_words.append(word)
+    return json.dumps(good_words)
